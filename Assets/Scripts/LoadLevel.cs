eac9d01d13d2b80c8c217b6f5ac6a620dfe88573
Igor Chigur <chigur.ig@gmail.com>
2020-12-25 23:00:27 +0200

    added screens: higscores, gamescreen, achievements. Added packages: Drawable, Animation, Scripts, Fonts. Added scripts: LoadLevel, Exit

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class LoadLevel : MonoBehaviour, IPointerDownHandler
+{
+    public string nextScene;
+    float maxLevels = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (nextScene == null || nextScene == "") {
+            nextScene = "Scene-" + loadOpenedLevels();
+        }
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void OnPointerDown(PointerEventData eventdata)
+    {
+        LoadNextLevel();
+    }
+
+    void LoadNextLevel()
+    {
+        Application.LoadLevel(nextScene);
+    }
+
+    public float loadOpenedLevels()
+    { 
+        float result = PlayerPrefs.GetFloat("openedLevels");
+        if (result >= maxLevels) {
+            result = maxLevels - 1;
+        }
+        return result;
+    }
+}
